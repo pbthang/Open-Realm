c9831cae48fdb57c4da0e368414d9bbc3fcd8953
Laridian <larby2409@gmail.com>
2021-06-13 17:55:25 +0800

    Revert "Revert "Add book db""
    This reverts commit 0f53878cff04d77564d3a7f611ee36d26f8b277e.

---
+++ server/app/models/book.model.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, Sequelize) => {
+  const Book = sequelize.define("book", {
+    title: {
+      type: Sequelize.STRING
+    },
+    author_id: {
+      type: Sequelize.INTEGER
+    },
+    description: {
+      type: Sequelize.TEXT
+    },
+    numberOfChapters: {
+      type: Sequelize.INTEGER
+    },
+    numberOfBookmarks: {
+      type: Sequelize.INTEGER
+    },
+    comments_id: {
+      type: Sequelize.ARRAY(Sequelize.INTEGER)
+    },
+    published: {
+      type: Sequelize.BOOLEAN
+    }
+  });
+
+  return Book;
+};
