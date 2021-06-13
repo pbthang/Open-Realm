23c764eb3b133ef21667fba2b2a49f7936bdb868
Laridian <larby2409@gmail.com>
2021-06-13 17:36:34 +0800

    Add book db

---
+++ server/app/routes/book.route.js
@@ -0,0 +1,25 @@
+module.exports = app => {
+  const books = require("../controllers/book.controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new Book
+  router.post("/", books.create);
+
+  // Retrieve all Books
+  router.get("/", books.findAll);
+
+  // Retrieve all published Books
+  router.get("/published", books.findAllPublished);
+
+  // Retrieve a single Book with id
+  router.get("/:id", books.findOne);
+
+  // Update a Book with id
+  router.put("/:id", books.update);
+
+  // Delete a Book with id
+  router.delete("/:id", books.delete);
+
+  app.use('/api/books', router);
+};
