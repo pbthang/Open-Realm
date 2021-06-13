255cd0e9d2ccef3ad7839e3ecaea4812543f8796
Laridian <larby2409@gmail.com>
2021-06-14 01:30:53 +0800

    Homepage is using books from api server
    Change Home into Component class, comment out old code

--- reactjs/src/pages/Home.js
+++ reactjs/src/pages/Home.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component} from "react";
 import AppShell from "../components/AppShell";
 import Post from "../components/Post";
-import { makeStyles } from "@material-ui/core/styles";
-import api from "../api/api";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import BookDataService from "../services/book.service";
 
-const useStyles = makeStyles({
+const styles = theme => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
@@ -13,31 +13,98 @@ const useStyles = makeStyles({
   container: {},
 });
 
-function Home() {
-  const classes = useStyles();
-  const [books, setBooks] = useState([]);
+// const useStyles = makeStyles({
+//   root: {
+//     display: "flex",
+//     flexWrap: "wrap",
+//     alignItems: "flex-start",
+//   },
+//   container: {},
+// });
 
-  const retrieveBooks = async () => {
-    const response = await api.get("/stories");
-    return response.data;
-  };
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
+    this.retrieveBooks = this.retrieveBooks.bind(this);
+    // this.refreshList = this.refreshList.bind(this);
+    // this.setActiveBook = this.setActiveBook.bind(this);
+    // this.removeAllBooks = this.removeAllBooks.bind(this);
+    this.searchTitle = this.searchTitle.bind(this);
 
-  useEffect(() => {
-    const getAllBooks = async () => {
-      const allBooks = await retrieveBooks();
-      if (allBooks) setBooks(allBooks);
+    this.state = {
+      books: [],
+      // currentBook: null,
+      // currentIndex: -1,
+      searchTitle: ""
     };
+  }
 
-    getAllBooks();
-  }, []);
+  componentDidMount() {
+    this.retrieveBooks();
+  }
 
-  return (
-    <AppShell className={classes.root}>
+  onChangeSearchTitle(e) {
+    const searchTitle = e.target.value;
+
+    this.setState({
+      searchTitle: searchTitle
+    });
+  }
+
+  retrieveBooks() {
+    BookDataService.getAll()
+      .then(response => {
+        this.setState({
+          books: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  searchTitle() {
+    BookDataService.findByTitle(this.state.searchTitle)
+      .then(response => {
+        this.setState({
+          books: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    const {books, searchTitle} = this.state;
+    const { classes } = this.props;
+    // const [books, setBooks] = useState([]);
+    //
+    // const retrieveBooks = async () => {
+    //   const response = await api.get("/stories");
+    //   return response.data;
+    // };
+    //
+    // useEffect(() => {
+    //   const getAllBooks = async () => {
+    //     const allBooks = await retrieveBooks();
+    //     if (allBooks) setBooks(allBooks);
+    //   };
+    //
+    //   getAllBooks();
+    // }, []);
+
+    return (
+      <AppShell className={classes.root}>
       {books.map((book, idx) => (
         <Post type="book" book={book} author={book.author} key={idx} />
       ))}
-    </AppShell>
-  );
+      </AppShell>
+    );
+  }
 }
 
-export default Home;
+export default withStyles(styles, { withTheme: true })(Home);
