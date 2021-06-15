9aeda9580588908a989a19c9464f7196781bfecd
unknown <bathang02@gmail.com>
2021-06-16 00:16:09 +0800

    minor fix

--- client/src/auth0MgmtAPIToken.js
+++ client/src/auth0MgmtAPIToken.js
@@ -4,14 +4,12 @@ const ACCESS_TOKEN = (async () => {
   const TOKEN = await axios.post(
     "https://dev-d1rzgdpx.jp.auth0.com/oauth/token",
     {
-      client_id: "BAhbwdqvE0H4AJsipKsGbd8oJnsKXujZ",
-      client_secret:
-        "xtdHTnZzQERByW4wmxM0lVnp1ns9oSa-NZSv2E35_93UQg9zxw3yzHCIkaHwt8QH",
+      client_id: process.env.REACT_APP_MGMT_API_CLIENT_ID,
+      client_secret: process.env.REACT_APP_MGMT_API_CLIENT_SECRET,
       audience: "https://dev-d1rzgdpx.jp.auth0.com/api/v2/",
       grant_type: "client_credentials",
     }
   );
-  console.log(TOKEN.data.access_token);
   return TOKEN.data.access_token;
 })();
 
