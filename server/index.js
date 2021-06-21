f7206c97375c80c7ba44d5405f835ed403cfd88e
unknown <bathang02@gmail.com>
2021-06-21 15:17:07 +0800

    add date

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ dotenv.config();
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000",
-};
+// var corsOptions = {
+//   origin: "http://localhost:3000",
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
