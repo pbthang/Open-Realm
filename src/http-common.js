1b9d8dd3ddaa5c30f6571b05b3bc5b99cade1ebe
Laridian <larby2409@gmail.com>
2021-06-24 16:49:33 +0800

    local server for dev env

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default axios.create({
-  baseURL: "https://openrealmapi.herokuapp.com/api",
+  baseURL: isProduction ? "https://openrealmapi.herokuapp.com/api" : "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
   },
