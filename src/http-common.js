109135137ceac17b396b71409b36ca67b162f323
Laridian <larby2409@gmail.com>
2021-06-27 17:18:43 +0800

    change api url

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const isProduction = process.env.REACT_APP_ENV === "production";
 
 export default axios.create({
-  baseURL: isProduction ? "https://openrealmapi.herokuapp.com/api" : "http://localhost:8080/api",
+  baseURL: isProduction ? "https://api.hedgeing.xyz/api" : "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
   },
